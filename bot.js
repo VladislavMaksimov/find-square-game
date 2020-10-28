14d7dfe8f15bbf7d4e23c574efdef468f491e054
VladislavMaksimov <workmaximov@yandex.ru>
2020-10-28 19:00:56 +0500

    Comments added.

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ const stopBot = () => {
     setPlaying(false)
 }
 
+// Сравнивает один блок с двумя контрольными
 const isBlockUnusual = (checkedBlock, controlBlock1, controlBlock2) => {
     const checkedBlockColor = checkedBlock.style.backgroundColor;
     const controlBlock1Color = controlBlock1.style.backgroundColor;
@@ -17,6 +18,7 @@ const isBlockUnusual = (checkedBlock, controlBlock1, controlBlock2) => {
     return checkedBlockColor !== controlBlock1Color && checkedBlockColor !== controlBlock2Color; 
 }
 
+// Ищет отличающийся по цвету блок среди трёх первых
 const findUnusualFromFirstThree = (block0, block1, block2) => {
     if (isBlockUnusual(block0, block1, block2)) {
         block0.click();
@@ -33,11 +35,14 @@ const findUnusualFromFirstThree = (block0, block1, block2) => {
     return false;
 }
 
+// Проходит уровни, если они отрендерены
 const tryToPlay = () => {
     if (!state.playing) return;
 
     const blocks = document.getElementsByClassName('field-block');
     if (blocks.length > 3) {
+        // Если среди трёх первых блоков нет нужного, то у них "обычный" цвет
+        // и достаточно сравнить остальные с их цветом
         if (!findUnusualFromFirstThree(blocks[0], blocks[1], blocks[2]))
             for (let i = 3; i < blocks.length; i++)
                 if(blocks[i].style.backgroundColor !== blocks[0].style.backgroundColor) {
@@ -46,15 +51,18 @@ const tryToPlay = () => {
                 }
     }
 
+    // Запускается каждую секунду
     setTimeout(tryToPlay, 1000);
 }
 
+// Запускает игру
 const tryToStart = () => {
     setPlaying(true);
 
     const startButton = document.getElementsByClassName('start-button')[0];
     const blocks = document.getElementsByClassName('field-block');
 
+    // Начинает играть только если блоки отрендерились, иначе нажимает на кнопку "старт"
     if (blocks.length > 0)
         tryToPlay();
     else {
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Общее время игры
 const initialTime = 25;
 
+// Текущее состояние игры
 const gameData = {
     level: 0,
     time: initialTime,
@@ -13,20 +15,24 @@ const clear = (element) => {
     element.textContent = '';
 }
 
+// Получает случайное значение между first и last
 const randomize = (first, last) => {
     return first + Math.floor(Math.random() * (last - first));
 }
 
+// Рендерит текущее время
 const setTime = () => {
     const timerNum = document.getElementsByClassName('timer-num')[0];
     timerNum.textContent = gameData.time;
 }
 
+// Получает r, g или b компонент "необычного" цвета по компоненту обычного
 const getColorComponent = (component) => {
     const additionalValue = gameData.level < 35 ? gameData.level : 35;
     return component / 100 * 2 * (additionalValue + 10);
 }
 
+// Получает "необычный" цвет по обычному
 const getUnusualColor = (r, g, b) => {
     const newR = getColorComponent(r);
     const newG = getColorComponent(g);
@@ -34,6 +40,7 @@ const getUnusualColor = (r, g, b) => {
     return `rgb(${newR}, ${newG}, ${newB})`
 }
 
+// Сериализует текущее состояние игры в json и сохраняет его в localStorage
 const serializeProgress = () => {
     const progress = {
         level: gameData.level,
@@ -45,6 +52,7 @@ const serializeProgress = () => {
     localStorage.setItem('progress', JSON.stringify(progress));
 }
 
+// Десериализует текущее состояние игры из json'a из localStorage
 const deserializeProgress = () => {
     const progress = JSON.parse(localStorage.getItem('progress'));
 
@@ -57,14 +65,18 @@ const deserializeProgress = () => {
     gameData.time = localStorage.getItem('time');
 }
 
+// Рендерит новый уровень
 const renderLevel = () => {
     const gameField = document.getElementsByClassName('game-field')[0];
     const levelNum = document.getElementsByClassName('level-num')[0];
     const blocksCount = gameData.sideLength * gameData.sideLength;
 
+    // Удаляет старые блоки
     clear(gameField);
+    // Рендерит номер уровня
     levelNum.textContent = gameData.level;
 
+    // Меняет количество блоков
     gameField.style.gridTemplateColumns = `repeat(${gameData.sideLength}, auto)`;
     gameField.style.gridTemplateRows = `repeat(${gameData.sideLength}, auto)`;
 
@@ -81,6 +93,7 @@ const renderLevel = () => {
 
 }
 
+// Получает текущее состояние игры
 const createGameData = () => {
     const level = gameData.level + 1;
     const sideLength = level >= 10 ? 10 : level + 1;
@@ -102,7 +115,9 @@ const createGameData = () => {
     renderLevel();
 }
 
+// Завершает игру
 const finishGame = () => {
+    // Останавливает бота и делает кнопку бота кликабельной
     stopBot();
     const botButton = document.getElementsByClassName('bot-button')[0];
     botButton.classList.remove('playing');
@@ -110,19 +125,22 @@ const finishGame = () => {
 
     alert(`Игра окончена! Уровней пройдено: ${gameData.level - 1}.`);
 
+    // Очищает данные
     localStorage.clear();
     gameData.level = 0;
     gameData.time = initialTime;
 
-    const levelNum = document.getElementsByClassName('level-num')[0];
-    const timerNum = document.getElementsByClassName('timer-num')[0];
-    clear(levelNum);
-    timerNum.textContent = initialTime;
-
+    // Очищает игровое поле
     const gameField = document.getElementsByClassName('game-field')[0];
     clear(gameField);
     gameField.style.gridTemplateColumns = '1fr';
     gameField.style.gridTemplateRows = '1fr';
+
+    // Рендерит исходное состояние игры
+    const levelNum = document.getElementsByClassName('level-num')[0];
+    const timerNum = document.getElementsByClassName('timer-num')[0];
+    clear(levelNum);
+    timerNum.textContent = initialTime;
 
     const startButton = document.createElement('div');
     startButton.className = 'start-button';
@@ -131,6 +149,7 @@ const finishGame = () => {
     gameField.appendChild(startButton);
 }
 
+// Запускает таймер
 const startTimer = () => {
     const delay = 1000;
     let timer = setTimeout(function tick(){
@@ -145,6 +164,7 @@ const startTimer = () => {
     }, delay);
 }
 
+// Запускает бота
 const useBot = () => {
     const botButton = document.getElementsByClassName('bot-button')[0];
     botButton.classList.add('playing');
@@ -152,6 +172,7 @@ const useBot = () => {
     startBot();
 }
 
+// Десериализует и рендерит данные из localStorage, запускает работу таймера
 const loadGame = () => {
     deserializeProgress();
     setTime();
@@ -159,6 +180,7 @@ const loadGame = () => {
     renderLevel();
 }
 
+// Начинает игру
 const start = (startButton) => {
     startButton.remove();
     startTimer();
@@ -169,6 +191,8 @@ window.addEventListener('load', () => {
     const botButton = document.getElementsByClassName('bot-button')[0];
     botButton.addEventListener('click', useBot);
 
+    // Если в localStorage что-то есть, загружает состояние игры из него
+    // иначе рендерит начальное состояние игры
     if (localStorage.getItem('progress') !== null)
         loadGame();
     else {
